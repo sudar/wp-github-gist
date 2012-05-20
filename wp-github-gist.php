9c6074ad4eb59af06238d8d69bbd1363cf46bea7
sudar <sudar@sudarmuthu.com>
2012-05-20 07:31:21 +0000

    Added the ability to choose gist-it server
    git-svn-id: http://plugins.svn.wordpress.org/wp-github-gist/trunk@546479 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-github-gist.php
+++ wp-github-gist.php
@@ -4,7 +4,7 @@ Plugin Name: WP Github Gist
 Plugin URI: http://sudarmuthu.com/wordpress/wp-github-gist
 Description: Embed files and gist from Github in your blog posts or pages.
 Author: Sudar
-Version: 0.2
+Version: 0.3
 Donate Link: http://sudarmuthu.com/if-you-wanna-thank-me
 License: GPL
 Author URI: http://sudarmuthu.com/
@@ -14,8 +14,11 @@ Text Domain: wp-github-gist
 2011-08-23 - v0.1 - Initial Release
 2012-05-02 - v0.2 - (Dev time: 0.5 hours)
                   - Fixed issue in embedding Github files
+2012-05-20 - v0.3 - (Dev time: 1 hour)
+                  - Added the ability to choose gist-it server
 
 Based on Github Gist Plugin http://wordpress.org/extend/plugins/github-gist by Jingwen Owen Ou
+Used the Gist-it script https://github.com/sudar/gist-it by Robert Krimen
 */
 
 /*  Copyright 2010  Sudar Muthu  (email : sudar@sudarmuthu.com)
@@ -39,8 +42,15 @@ Based on Github Gist Plugin http://wordpress.org/extend/plugins/github-gist by J
  */
 class WPGithubGist {
 
+    // menu slug
+    const MENU_SLUG = 'wp-github-gist';
+
+    // gist
     const REGEXP_GIST_URL = "\"https:\/\/gist.github.com\/(.+)\.js\?file=(.+)\"";
-    const GIST_IT_SERVER = "http://gist-it.sudarmuthu.com/github"; //TODO: Make this a configurable parameter
+
+    // github
+    const GIST_IT_SERVER = "http://gist-it.sudarmuthu.com";
+    const GITHUB = "/github";
 
     /**
      * Initalize the plugin by registering the hooks
@@ -50,9 +60,123 @@ class WPGithubGist {
         // Load localization domain
         load_plugin_textdomain( 'wp-github-gist', false, dirname(plugin_basename(__FILE__)) . '/languages' );
 
+        // Settings hooks
+        add_action( 'admin_menu', array(&$this, 'register_settings_page') );
+        add_action( 'admin_init', array(&$this, 'add_settings') );
+
         // register short code
         add_shortcode('gist', array(&$this, 'gist_shortcode_handler'));
         add_shortcode('github', array(&$this, 'github_shortcode_handler'));
+
+        // add action links
+        $plugin = plugin_basename(__FILE__);
+        add_filter("plugin_action_links_$plugin", array(&$this, 'add_action_links'));
+    }
+
+    /**
+     * Register the settings page
+     */
+    function register_settings_page() {
+        add_options_page( __('wp-github-gist', 'wp-github-gist'), __('wp-github-gist', 'wp-github-gist'), 'manage_options', self::MENU_SLUG, array(&$this, 'settings_page') );
+    }
+
+    /**
+     * add options
+     */
+    function add_settings() {
+        // Register options
+        register_setting( 'wp-github-gist-options', 'wp-github-gist-options', array(&$this, 'validate_settings'));
+
+        //Add default Options section
+        add_settings_section('wgg_global_section', '', array(&$this, 'wgg_global_section_text'), self::MENU_SLUG);
+
+		// add setting fields
+        add_settings_field('gist-it-server', __('Gist it Server', 'wp-github-gist'), array(&$this, 'wgg_gist_it_server_callback'), self::MENU_SLUG, 'wgg_global_section');
+    }
+
+    /**
+     * Dipslay the Settings page
+     */
+    function settings_page() {
+?>
+        <div class="wrap">
+            <?php screen_icon(); ?>
+            <h2><?php _e( 'wp-github-gist Settings', 'wp-github-gist' ); ?></h2>
+
+            <iframe height = "950" src = "http://sudarmuthu.com/projects/wordpress/wp-github-gist/sidebar.php?color=<?php echo get_user_option('admin_color'); ?>"></iframe>
+
+			<div style = "float:left; width:75%">
+				<form id="smer_form" method="post" action="options.php">
+					<?php settings_fields('wp-github-gist-options'); ?>
+					<?php do_settings_sections(self::MENU_SLUG); ?>
+
+					<p class="submit">
+						<input type="submit" name="wp-github-gist-submit" class="button-primary" value="<?php _e('Save Changes', 'wp-github-gist') ?>" />
+					</p>
+				</form>
+			</div>
+        </div>
+<?php
+        // Display credits in Footer
+        add_action( 'in_admin_footer', array(&$this, 'add_footer_links'));
+    }
+
+    /**
+     * Adds Footer links.
+     *
+     * Based on http://striderweb.com/nerdaphernalia/2008/06/give-your-wordpress-plugin-credit/
+     */
+    function add_footer_links() {
+        $plugin_data = get_plugin_data( __FILE__ );
+        printf('%1$s ' . __("plugin", 'wp-github-gist') .' | ' . __("Version", 'wp-github-gist') . ' %2$s | '. __('by', 'wp-github-gist') . ' %3$s<br />', $plugin_data['Title'], $plugin_data['Version'], $plugin_data['Author']);
+    }
+
+    /**
+     * hook to add action links
+     * @param <type> $links
+     * @return <type>
+     */
+    function add_action_links( $links ) {
+        // Add a link to this plugin's settings page
+        $settings_link = '<a href="options-general.php?page=' . self::MENU_SLUG . '">' . __("Settings", 'wp-github-gist') . '</a>';
+        array_unshift( $links, $settings_link );
+        return $links;
+    }
+
+    /**
+     * Validate the options entered by the user
+     *
+     * @param <type> $input
+     * @return <type>
+     */
+    function validate_settings($input) {
+        $input['gist-it-server'] = esc_url($input['gist-it-server'], array('http', 'https'));
+        return $input;
+    }
+
+    /**
+     * Print global section text
+     */
+    function  wgg_global_section_text() {
+		// Empty as of now
+    }
+
+	/**
+	 * Callback for printing Gist it server Setting
+	 *
+	 * @return void
+	 * @author Sudar
+	 */
+    function wgg_gist_it_server_callback() {
+        $options = get_option('wp-github-gist-options');
+
+        if (empty($options['gist-it-server'])) {
+            $options['gist-it-server'] = self::GIST_IT_SERVER;
+        }
+
+        echo "<input id='gist-it-server' name='wp-github-gist-options[gist-it-server]' size='40' type='text' value='{$options['gist-it-server']}' ><br>";
+        _e('By default <code>http://gist-it.sudarmuthu.com</code> server will be used', 'wp-github-gist');
+        _e('But you can also setup your own gist-it server and use it. Instructions on how to deploy your own gist-it server, can be found in the <a href = "http://sudarmuthu.com/wordpress/wp-github-gist#settings">Plugins homepage</a>', 'wp-github-gist');
     }
 
     /**
@@ -112,7 +236,13 @@ class WPGithubGist {
      * @return <type>
      */
     private function get_github_embed_script($file, $start_line = 0, $end_line = 0) {
-        $script = '<script src = "' . self::GIST_IT_SERVER . $file;
+
+        $options = get_option('wp-github-gist-options');
+        if (empty($options['gist-it-server'])) {
+            $options['gist-it-server'] = self::GIST_IT_SERVER;
+        }
+
+        $script = '<script src = "' . $options['gist-it-server'] . self::GITHUB . $file;
 
         if ($start_line > 0 || $end_line > 0) {
             $script .= "?slice=$start_line:$end_line&footer=minimal";
